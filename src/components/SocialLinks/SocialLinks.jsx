b1fdc28da1a492e1b01840b682b3da7ca7294a30
Kasinath Mandal <kasi21.12.2000@gmail.com>
2025-07-04 00:14:25 +0530

    nav updated

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,19 +1,98 @@
+// // components/UI/SocialLinks.jsx
+// import React from "react";
+// import { Box, IconButton, Tooltip } from "@mui/material";
+// import {
+//   LinkedIn,
+//   GitHub,
+//   Facebook,
+//   Instagram,
+//   Twitter,
+//   YouTube,
+//   Telegram,
+//   WhatsApp,
+// } from "@mui/icons-material";
+
+// const socialPlatforms = [
+
+//   {
+//     name: "Facebook",
+//     icon: <Facebook />,
+//     link: "https://www.facebook.com/profile.php?id=100066637445017",
+//     color: "#1877F2",
+//   },
+//   {
+//     name: "Instagram",
+//     icon: <Instagram />,
+//     link: "https://www.instagram.com/chaukhat_25/",
+//     color: "#E1306C",
+//   },
+//   {
+//     name: "Twitter",
+//     icon: <Twitter />,
+//     link: "https://x.com/chaukhat_25",
+//     color: "#1DA1F2",
+//   },
+//   {
+//     name: "YouTube",
+//     icon: <YouTube />,
+//     link: "https://youtube.com/@thehindisansar?si=D7N6lmZoP4anXy6Q",
+//     color: "#FF0000",
+//   },
+//   {
+//     name: "Telegram",
+//     icon: <Telegram />,
+//     link: "https://t.me/Chaukhat_25",
+//     color: "#0088cc",
+//   },
+//   {
+//     name: "WhatsApp",
+//     icon: <WhatsApp />,
+//     link: "https://chat.whatsapp.com/GzuDwn1TPbdBW6wM5NnmMu",
+//     color: "#25D366",
+//   },
+// ];
+
+// const SocialLinks = () => {
+//   return (
+//     <Box display="flex" gap={2} flexWrap="wrap">
+//       {socialPlatforms.map((platform) => (
+//         <Tooltip key={platform.name} title={platform.name}>
+//           <IconButton
+//             component="a"
+//             href={platform.link}
+//             target="_blank"
+//             rel="noopener"
+//             sx={{
+//               color: platform.color,
+//               transition: "transform 0.2s",
+//               "&:hover": { transform: "scale(1.2)" },
+//             }}
+//           >
+//             {platform.icon}
+//           </IconButton>
+//         </Tooltip>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// export default SocialLinks;
+
+
 // components/UI/SocialLinks.jsx
 import React from "react";
 import { Box, IconButton, Tooltip } from "@mui/material";
 import {
-  LinkedIn,
-  GitHub,
   Facebook,
   Instagram,
   Twitter,
   YouTube,
   Telegram,
   WhatsApp,
+  Email,
 } from "@mui/icons-material";
 
 const socialPlatforms = [
-
   {
     name: "Facebook",
     icon: <Facebook />,
@@ -50,18 +129,28 @@ const socialPlatforms = [
     link: "https://chat.whatsapp.com/GzuDwn1TPbdBW6wM5NnmMu",
     color: "#25D366",
   },
+  {
+    name: "Email",
+    icon: <Email />,
+    link: "mailto:chaukhatpatrika@gmail.com",
+    color: "#D44638", // Gmail red
+  },
 ];
 
 const SocialLinks = () => {
   return (
-    <Box display="flex" flexWrap="wrap">
+    <Box
+      display="flex"
+      flexWrap="wrap"
+      justifyContent={{ xs: "center", sm: "flex-start" }}
+    >
       {socialPlatforms.map((platform) => (
         <Tooltip key={platform.name} title={platform.name}>
           <IconButton
             component="a"
             href={platform.link}
-            target="_blank"
-            rel="noopener"
+            target={platform.link.startsWith("mailto:") ? undefined : "_blank"}
+            rel={platform.link.startsWith("mailto:") ? undefined : "noopener"}
             sx={{
               color: platform.color,
               transition: "transform 0.2s",
